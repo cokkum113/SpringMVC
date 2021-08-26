836dfed08b01e65c8253272dc1be698982aa9b50
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-08-26 19:27:31 +0900

    using Controller before using MVC

--- SpringMVCProject/src/main/java/hello/servelet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ SpringMVCProject/src/main/java/hello/servelet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import hello.servelet.web.frontcontroller.v3.ControllerV3;
 import hello.servelet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servelet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servelet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servelet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servelet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servelet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servelet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servelet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -31,17 +35,24 @@ public class FrontControllerServletV5 extends HttpServlet {
     }
 
     private void initHandlerMappingMap() {
-        handlerMappingMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        handlerMappingMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        handlerMappingMap.put("/front-controller/v3/members", new MemberListControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        //v4추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         Object handler = getHandler(request);
 
         if (handler == null) {
@@ -49,19 +60,10 @@ public class FrontControllerServletV5 extends HttpServlet {
             return;
         }
 
-        MyHandlerAdapter a;
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
 
-        for (MyHandlerAdapter adapter : handlerAdapters) {
-            if (adapter.support(handler)) {
-                a = adapter;
-            }
-        }
+        ModelView mv = adapter.handle(request, response, handler);
 
-        //paraMap을 넘겨줘야함
-
-        Map<String, String> paraMap = createParaMap(request);
-
-        ModelView mv = controller.process(paraMap);
 
         String viewName = mv.getViewName();//논리이름 new-form
         MyView view = viewResolver(viewName);
@@ -69,8 +71,21 @@ public class FrontControllerServletV5 extends HttpServlet {
         view.render(mv.getModel(), request, response);
     }
 
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.support(handler)) {
+                return adapter;
+            }
+        }
+        throw  new IllegalArgumentException("handler adapter를 찾을 수 없습니다.");
+    }
+
     private Object getHandler(HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         return handlerMappingMap.get(requestURI);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
